74f4ec97f70a8c2164497e6f3200a76e6cc377bd
Richard Emate <richardemate@gmail.com>
2020-04-13 20:31:19 +0100

    fixing my lint errors

--- src/estimator.js
+++ src/estimator.js
@@ -1,35 +1,37 @@
 const estimateInfection = (period, typeOfPeriod, currentNum) => {
-    let convertedPeriod;
-    if( typeOfPeriod === 'months') {
-        convertedPeriod = period * 30;
-    } else if(typeOfPeriod === 'weeks') {
-        convertedPeriod = period * 7;
-    } else if(typeOfPeriod === 'days') {
-        convertedPeriod = period
-    }
+  let convertedPeriod;
+  if (typeOfPeriod === 'months') {
+    convertedPeriod = period * 30;
+  } else if (typeOfPeriod === 'weeks') {
+    convertedPeriod = period * 7;
+  } else if (typeOfPeriod === 'days') {
+    convertedPeriod = period;
+  }
 
-    let factor = Math.trunc(convertedPeriod / 3);
+  const factor = Math.trunc(convertedPeriod / 3);
 
-    return currentNum * (2 ** factor);
-}
+  return currentNum * (2 ** factor);
+};
 
 const covid19ImpactEstimator = (data) => {
-    let impact = {}
-    let severeImpact = {}
-    let impactCurrentlyInfected = data.reportedCases * 10;
-    let sevImpactCurrentlyInfected = data.reportedCases *  50;
+  const impact = {};
+  const severeImpact = {};
+  const impactCurrentlyInfected = data.reportedCases * 10;
+  const sevImpactCurrentlyInfected = data.reportedCases * 50;
 
-    impact.currentlyInfected = impactCurrentlyInfected;
-    severeImpact.currentlyInfected = sevImpactCurrentlyInfected;
-    
-    impact.infectionsByRequestedTime = estimateInfection(data.timeToElapse, data.periodType, impactCurrentlyInfected);
-    severeImpact.infectionsByRequestedTime = estimateInfection(data.timeToElapse, data.periodType, sevImpactCurrentlyInfected);
+  impact.currentlyInfected = impactCurrentlyInfected;
+  severeImpact.currentlyInfected = sevImpactCurrentlyInfected;
 
-    return {
-        data = data,
-        impact = impact,
-        severeImpact = severeImpact
-    }
+  impact.infectionsByRequestedTime = estimateInfection(data.timeToElapse, data.periodType,
+    impactCurrentlyInfected);
+  severeImpact.infectionsByRequestedTime = estimateInfection(data.timeToElapse,
+    data.periodType, sevImpactCurrentlyInfected);
+
+  return {
+    data,
+    impact,
+    severeImpact
+  };
 };
 
 export default covid19ImpactEstimator;
