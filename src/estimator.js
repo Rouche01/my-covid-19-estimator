187b295db7714d9a58015f6c144219b95949b69b
Richard Emate <richardemate@gmail.com>
2020-04-13 20:06:13 +0100

    my first go at challenge one

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,35 @@
-const covid19ImpactEstimator = (data) => {};
+const estimateInfection = (period, typeOfPeriod, currentNum) => {
+    let convertedPeriod;
+    if( typeOfPeriod === 'months') {
+        convertedPeriod = period * 30;
+    } else if(typeOfPeriod === 'weeks') {
+        convertedPeriod = period * 7;
+    } else if(typeOfPeriod === 'days') {
+        convertedPeriod = period
+    }
+
+    let factor = Math.trunc(convertedPeriod / 3);
+
+    return currentNum * (2 ** factor);
+}
+
+const covid19ImpactEstimator = (data) => {
+    let impact = {}
+    let severeImpact = {}
+    let impactCurrentlyInfected = data.reportedCases * 10;
+    let sevImpactCurrentlyInfected = data.reportedCases *  50;
+
+    impact.currentlyInfected = impactCurrentlyInfected;
+    severeImpact.currentlyInfected = sevImpactCurrentlyInfected;
+    
+    impact.infectionsByRequestedTime = estimateInfection(data.timeToElapse, data.periodType, impactCurrentlyInfected);
+    severeImpact.infectionsByRequestedTime = estimateInfection(data.timeToElapse, data.periodType, sevImpactCurrentlyInfected);
+
+    return {
+        data = data,
+        impact = impact,
+        severeImpact = severeImpact
+    }
+};
 
 export default covid19ImpactEstimator;
